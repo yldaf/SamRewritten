58c3ee8a74188a853f595025a39f8958f1f69713
Mike Frysinger <vapier@gentoo.org>
2021-12-23 00:48:24 -0500

    fix quoting in scripts

--- bin/launch.sh
+++ bin/launch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-SCRIPT_PATH=`dirname $(realpath $0)`
+SCRIPT_PATH=$(dirname "$(realpath "$0")")
 export LD_LIBRARY_PATH=${SCRIPT_PATH}
 
 # Workaround waiting for a fix https://github.com/lloyd/yajl/issues/222
 export LC_NUMERIC=en_US.UTF-8
 
-cd ${SCRIPT_PATH}/..
-${SCRIPT_PATH}/samrewritten $@
+cd "${SCRIPT_PATH}"/..
+"${SCRIPT_PATH}"/samrewritten "$@"
